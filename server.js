a1da0c19cb376a4e8dfcf910297efb9acaedf1ae
Gian Banares <gianbanares@gb-mbp.attlocal.net>
2019-01-14 21:12:49 -0800

    commit

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public"));
+app.use(express.json  ());
+app.use(express.static("  public"));
 
 // Handlebars
 app.engine(
-  "handlebars",
+  'handlebars',
   exphbs({
     defaultLayout: "main"
   })
 );
-app.set("view engine", "handlebars");
+app.set('view engine', 'handlebars');
 
 // Routes
 require("./routes/apiRoutes")(app);
