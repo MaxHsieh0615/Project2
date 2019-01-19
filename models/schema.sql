a6d04f4a2124e91cdb36fad8ae2d6ed25e618d96
richardluong127 <richardluong127@gmail.com>
2019-01-19 13:24:27 -0800

    schema and blank readme

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,33 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS jobs_piggieDB;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE jobs_piggieDB;
+
+USE jobs_piggieDB;
+
+CREATE TABLE job_name (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(65) NOT NULL,
+  description VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE piggy_bank (
+  id INT NOT NULL AUTO_INCREMENT,
+  earnings DECIMAL(10,2) NOT NULL,
+  description VARCHAR(255) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE child_user (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(20) NOT NULL,
+  user_password VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE adult_user (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(20) NOT NULL,
+  user_password VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
